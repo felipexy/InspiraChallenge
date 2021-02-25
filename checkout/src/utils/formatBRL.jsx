07f3ad87b92e2b00d2572de408863c5343606f5c
felipe <felipexy@hotmail.com>
2021-02-25 02:52:55 -0300

    currency

--- checkout/src/utils/formatBRL.jsx
+++ checkout/src/utils/formatBRL.jsx
@@ -6,7 +6,7 @@ function formatValues(value) {
   let x1 = x[0];
   const x2 = x.length > 1 ? `,${x[1]}` : '';
   while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, '$1' + '.' + '$2');
+    x1 = x1.replace(rgx, '$1.$2');
   }
   return x1 + x2;
 }
